a971dbedc8c08cfd9ad7059d046026f3e620215c
JyotiSharma28 <133893780+JyotiSharma28@users.noreply.github.com>
2023-08-22 15:39:13 +0530

    Update db.js
    added mongoDB atlas URI

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose')
-const mongoURI="mongodb://localhost:27017/inotebook"
+// const mongoURI="mongodb://localhost:27017/inotebook"
+const mongoURI="mongodb+srv://sjyoti37796:XKY8KH9H0XaHMy33@cluster0.akfrg1n.mongodb.net/?retryWrites=true&w=majority"
 
 const connectToMongo=async()=>{
     mongoose.connect(mongoURI)
